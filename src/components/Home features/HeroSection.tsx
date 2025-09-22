4bd26aee39cc3bc10942b28580c4c40cc974dbc6
Peter karanja <peterkaranja60@gmail.com>
2025-09-22 04:18:18 -0500

    Refactor code

--- src/components/Home features/HeroSection.tsx
+++ src/components/Home features/HeroSection.tsx
@@ -5,7 +5,7 @@ export default function HeroSection() {
     <>
       <section
         id="home"
-        className="w-full h-screen flex flex-col lg:flex-row justify-center pt-16 lg:pt-28 pb-16 lg:pb-28 gap-8 lg:gap-20 px-4 sm:px-8 lg:px-16 bg-pink-500"
+        className="w-full h-screen flex flex-col lg:flex-row justify-center pt-16 lg:pt-28 pb-16 lg:pb-28 gap-8 lg:gap-20 px-4 sm:px-8 lg:px-16 "
       >
         HeroSection
       </section>

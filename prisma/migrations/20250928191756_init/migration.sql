4fefd81f848fa7bbd86e5a55e58adcba774a3ef8
samuelnmu <samuelnmu@gmail.com>
2025-09-28 22:37:54 +0300

    SQL database setup

--- prisma/migrations/20250928191756_init/migration.sql
+++ prisma/migrations/20250928191756_init/migration.sql
@@ -1,13 +1,11 @@
 -- CreateTable
-CREATE TABLE "public"."Contact" (
-    "id" SERIAL NOT NULL,
+CREATE TABLE "Contact" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "firstName" TEXT NOT NULL,
     "lastName" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "phone" TEXT NOT NULL,
     "service" TEXT NOT NULL,
     "message" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Contact_pkey" PRIMARY KEY ("id")
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
